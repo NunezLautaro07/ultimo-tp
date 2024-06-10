fc96c7acb299a41478ff61def1aba1b17d971e91
laununez07 <lautife007@gmail.com>
2024-06-10 14:33:33 -0300

    saco innecesarios

--- index.js
+++ index.js
@@ -9,10 +9,9 @@ const PORT = process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 mongoose
-  .connect(process.env.MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
+  .connect(process.env.MONGO_URI, 
+   
+  )
   .then(() => {
     console.log("Conectado a la base de datos");
   })
